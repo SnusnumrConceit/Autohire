a4af2f41c14e10fa5fc4d24a20108742d2139f53
SnusnumrConceit <snusnumr1996@gmail.com>
2018-03-29 05:09:45 +0400

    Add Validate Registration JS

---
+++ Scripts/registration_scripts.js
@@ -0,0 +1,162 @@
+$('#btn-registration').click(function () {  
+var login = $('#user-login').val(),
+        pass = $('#user-password').val(),
+        confirmPass = $('#user-confirm-password').val(),
+        lastName = $('#user-last-name').val(),
+        firstName = $('#user-first-name').val(),
+        middleName = $('#user-middle-name').val(),
+        user = new User(login, pass, lastName, firstName, middleName);
+        if (CheckData(user)) {
+            user = JSON.stringify(user);
+            $.post('admin/users.php', {user: user}, function (response) { 
+                if (response.length != 0) {
+                    alert(response);
+                } else {
+                    window.location.reload();                
+                }
+            })
+        }
+
+    function User(login, pass, lastName, firstName, middleName) { 
+        this.login = login,
+        this.pass = pass,
+        this.lastName = lastName,
+        this.firstName = firstName,
+        this.middleName = middleName
+     }
+
+    function CheckData(user) {  
+        try {
+            if (user.login !== undefined && user.login !== null && user.login.length !== 0 &&
+                user.pass !== undefined && user.pass !== null && user.pass.length !== 0 &&
+                user.lastName !== undefined && user.lastName !== null && user.lastName.length !== 0 &&
+                user.firstName !== undefined && user.firstName !== null && user.firstName.length !== 0 &&
+                user.middleName !== undefined && user.middleName !== null && user.middleName.length !== 0 &&
+                confirmPass !== undefined && confirmPass !== null && confirmPass.length !== 0) {
+                    if (user.login.length >= 6 && user.login.length <= 24 &&
+                        user.pass.length >= 6 && user.pass.length <= 24 &&
+                        user.lastName.length >= 3 && user.lastName.length <= 30 &&
+                        user.firstName.length >= 4 && user.firstName.length <= 15 &&
+                        user.middleName.length >= 6 && user.middleName.length <= 24) {
+                            if ((/[A-Za-z]{1,}[a-zA-Z0-9_.]{5,}/.exec(user.login) !== null)
+                                && (/[A-Za-z]{1,}[a-zA-Z0-9_.]{5,}/.exec(user.pass) !== null)
+                                && (/([A-Z][a-z]{2,})|([А-Я][a-я]{2,})/.exec(user.lastName) !== null)
+                                && (/([A-Z][a-z]{3,})|([А-Я][a-я]{3,})/.exec(user.firstName) !== null)
+                                && (/([A-Z][a-z]{5,})|([А-Я][a-я]{5,})/.exec(user.middleName) !== null)) {
+                                    if ((/[A-Za-z]{1,}[a-zA-Z0-9_.]{5,}/.exec(user.login)[0] === user.login)
+                                        && (/[A-Za-z]{1,}[a-zA-Z0-9_.]{5,}/.exec(user.pass)[0] === user.pass)
+                                        && (/([A-Z][a-z]{2,})|([А-Я][a-я]{2,})/.exec(user.lastName)[0] === user.lastName)
+                                        && (/([A-Z][a-z]{3,})|([А-Я][a-я]{3,})/.exec(user.firstName)[0] === user.firstName)
+                                        && (/([A-Z][a-z]{5,})|([А-Я][a-я]{5,})/.exec(user.middleName)[0] === user.middleName)) {
+                                            console.log(/([A-Z][a-z]{2,})|([А-Я][a-я]{2,})/.exec(user.lastName));
+                                            console.log(/([A-Z][a-z]{3,})|([А-Я][a-я]{3,})/.exec(user.firstName));
+                                            console.log(/([A-Z][a-z]{5,})|([А-Я][a-я]{5,})/.exec(user.middleName));
+                                            if (user.pass === confirmPass) {
+                                                return true;    
+                                            } else {
+                                                throw new Error('Uncomparable Passwords Error');
+                                            }
+                                    } else {
+                                        throw new Error('Wrong Data Error');
+                                    }   
+                            } else {
+                                throw new Error('Wrong Data Error');
+                            }                            
+                    } else {
+                        throw new Error('Length Data Error');
+                    }
+            } else {
+                throw new Error('Empty Data Error');
+            }
+        } catch (error) {
+            console.log(/[A-Za-z]{1,}[a-zA-Z0-9_.]{5,}/.exec(user.login));
+                console.log(/[A-Za-z]{1,}[a-zA-Z0-9_.]{5,}/.exec(user.pass));
+                console.log(/[A-ZА-ЯЁ]{1}[a-zа-яё]{2,}/.exec(user.lastName));
+                console.log(/[A-ZА-ЯЁ]{1}[a-zа-яё]{3,}/.exec(user.firstName));
+                console.log(/[A-ZА-ЯЁ]{1}[a-zа-яё]{5,}/.exec(user.middleName));
+                console.log('-------------------------------------------------');
+            if (error.message === 'Empty Data Error') {
+                if (user.login === undefined || user.login === null || user.login.length === 0) {
+                    alert('Вы не ввели логин!');
+                }
+
+                if (user.pass === undefined || user.pass === null || user.pass.length === 0) {
+                    alert('Вы не ввели пароль!');
+                }
+
+                if (user.lastName === undefined || user.lastName === null || user.lastName.length === 0) {
+                    alert('Вы не ввели фамилию!');
+                }
+
+                if (user.firstName === undefined || user.firstName === null || user.firstName.length === 0) {
+                    alert('Вы не ввели имя!');
+                }
+
+                if (user.middleName === undefined || user.middleName === null || user.middleName.length === 0) {
+                    alert('Вы не ввели отчество!');
+                }
+
+                if (confirmPass === undefined || confirmPass === null || confirmPass.length === 0) {
+                    alert('Вы не подтвердили пароль!');
+                }
+            }
+            
+            if (error.message === 'Length Data Error') {
+                
+                if (user.login.length < 6 || user.login.length > 24) {
+                    alert('Длина логина должна быть от 6 до 24 символов!');
+                }
+
+                if (user.pass.length < 6 || user.pass.length > 24) {
+                    alert('Длина пароля должна быть от 6 до 24 символов!');
+                }
+
+                if (user.lastName.length < 3 || user.lastName.length > 30) {
+                    alert('Длина фамилии должна быть от 3 до 30 символов!');
+                }
+
+                if (user.firstName.length < 4 || user.firstName.length > 15) {
+                    alert('Длина имени должна быть от 4 до 15 символов!');
+                }
+
+                if (user.middleName.length < 6 || user.middleName.length > 24) {
+                    alert('Длина отчества должна быть от 6 до 24 символов!');
+                }
+            }
+            
+            if (error.message === 'Wrong Data Error') {
+                if ((/[A-Za-z]{1,}[a-zA-Z0-9_.]{5,}/.exec(user.login) === null)){                    
+                    alert('Логин должен состоять из латинских букв, точки и нижнего подчёркивания!');
+                } else {
+                    if (/[A-Za-z]{1,}[a-zA-Z0-9_.]{5,}/.exec(user.login)[0] !== user.login) {
+                        alert('Логин должен состоять из латинских букв, точки и нижнего подчёркивания!');
+                    }
+                }
+                
+                if (/[A-Za-z]{1,}[a-zA-Z0-9_.]{5,}/.exec(user.pass) === null || (/[A-Za-z]{1,}[a-zA-Z0-9_.]{5,}/.exec(user.pass)[0] !== user.pass)){
+                    alert('Пароль должен состоять из латинских букв, точки и нижнего подчёркивания!');
+                }
+
+                if (/([A-Z][a-z]{2,})|([А-Я][a-я]{2,})/.exec(user.lastName) === null) {
+                    alert('Фамилия должна состоять из латинских букв или кириллистических букв!');
+                } else {
+                    if (/([A-Z][a-z]{2,})|([А-Я][a-я]{2,})/.exec(user.lastName)[0] !== user.lastName) {
+                        alert('Фамилия должна состоять из латинских букв или кириллистических букв!');
+                    }
+                }
+
+                if ((/([A-Z][a-z]{3,})|([А-Я][a-я]{3,})/.exec(user.firstName) === null) || (/([A-Z][a-z]{3,})|([А-Я][a-я]{3,})/.exec(user.firstName)[0] != user.firstName)) {
+                    alert('Имя должно состоять из латинских букв или кириллистических букв!');
+                }
+
+                if ((/([A-Z][a-z]{5,})|([А-Я][a-я]{5,})/.exec(user.middleName) === null) || (/([A-Z][a-z]{5,})|([А-Я][a-я]{5,})/.exec(user.middleName)[0] !== user.middleName)) {
+                    alert('Отчество должно состоять из латинских букв или кириллистических букв!')
+                }
+            }
+
+            if (error.message === 'Uncomparable Passwords Error') {
+                alert("Пароли не совпадают!");
+            }
+        }
+    }
+})
---
+++ header.php
@@ -0,0 +1,115 @@
+<?php
+
+//здесь будем мутить тему с куки
+
+//напиши стили для нав-табов: сам текст красный на белом фоне, при :hover и :active - красный фон, белые буквы
+?>
+<header class="header">
+    <nav class="navbar navbar-expand-sm bg-light">
+        <ul class="navbar-nav">
+        <?php                        
+            /*if (isset($current_user)) {
+              print <<<LOGGED_PANEL
+                <li class="nav-item"><a class="nav-link" href="Index/user.php?user={$student}">$current_user->Login</a></li>
+                <li class="nav-item"><a class="nav-link" href="Index/Logout.php">Выйти</a></li>    
+LOGGED_PANEL;
+            } else {
+              print <<<UNLOGGED_PANEL
+                <li class="nav-item" data-toggle="modal" data-target="#loginModal"><a class="nav-link" href="#">Войти</a></li>
+                <li class="nav-item" data-toggle="modal" data-target="#regModal"><a class="nav-link" href="#">Регистрация</a></li>
+UNLOGGED_PANEL;
+            }         
+         */   
+        ?>
+          <li class="nav-item" data-toggle="modal" data-target="#loginModal"><a class="nav-link" href="#">Войти</a></li>
+          <li class="nav-item" data-toggle="modal" data-target="#regModal"><a class="nav-link" href="#">Регистрация</a></li>
+        </ul>
+    </nav>
+        <div class="logo"><img src="сюда воткнёшь ссылку на лого" alt="АвтоПрокат"></div>
+    
+</header>
+
+<!-- Модальное окно авторизации -->
+<div class="modal fade" id="loginModal">
+  <div class="modal-dialog">
+    <div class="modal-content">
+
+      <!-- Модальная шапка -->
+      <div class="modal-header">
+        <h4 class="modal-title">Авторизация</h4>
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+      </div>
+
+      <!-- Модальное тело -->
+      <div class="modal-body">
+        <form method="POST">
+          <div class="form-group">
+            <label for="auth-login">Логин</label>
+            <input type="text" name="auth-login" id="auth-login" class="form-control">
+          </div>
+          <div class="form-group">
+            <label for="auth-pass">Пароль</label>
+            <input type="password" name="auth-pass" id="auth-pass" class="form-control">
+          </div>
+        </form>
+      </div>
+
+      <!-- Модальный футер -->
+      <div class="modal-footer">
+        <button type="button" class="btn btn-primary" id="btn-login">Войти</button>
+      </div>
+
+    </div>
+  </div>
+</div>
+
+<!-- Модальное окно регистрации -->
+<div class="modal fade" id="regModal">
+  <div class="modal-dialog">
+    <div class="modal-content">
+
+      <!-- Модальная шапка -->
+      <div class="modal-header">
+        <h4 class="modal-title">Регистрация</h4>
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+      </div>
+
+      <!-- Модальное тело -->
+      <div class="modal-body">
+        <form method="POST">
+            <div class="form-group">
+                <label for="user-login">Логин</label>
+                <input type="text" name="user-login" id="user-login" value="" class="form-control">
+            </div>
+            <div class="form-group">
+                <label for="user-password">Пароль</label>
+                <input type="password" name="user-pass" id="user-password" value="" class="form-control">
+            </div>
+            <div class="form-group">
+                <label for="user-confirm-password">Подтвердите пароль</label>
+                <input type="password" name="user-confirm-password" id="user-confirm-password" value="" class="form-control">
+            </div>
+            <div class="form-group">
+                <label for="user-last-name">Фамилия</label>
+                <input type="text" name="user-last-name" id="user-last-name" value="" class="form-control">
+            </div> 
+            <div class="form-group">
+                <label for="user-first-name">Имя</label>
+                <input type="text" name="user-first-name" id="user-first-name" value="" class="form-control">
+            </div>
+            <div class="form-group">
+                <label for="user-middle-name">Отчество</label>
+                <input type="text" name="user-middle-name" id="user-middle-name" value="" class="form-control">
+            </div>
+            
+        </form>
+      </div>
+
+      <!-- Модальный футер -->
+      <div class="modal-footer">
+        <button type="button" class="btn btn-primary" id="btn-registration">Зарегистрироваться</button>
+      </div>
+
+    </div>
+  </div>
+</div>
